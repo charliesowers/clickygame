984520ef2f1545ae55372b58dfbc8262fc4bf3af
CSowers <charliesow@gmail.com>
2018-10-25 19:22:26 -0400

    Updates

---
+++ precache-manifest.9a6eb9579c9a82b2cf6d2c1b9364f790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "37db3677dc3caf53e5c2",
+    "url": "/clickygame/static/js/main.37db3677.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/clickygame/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "37db3677dc3caf53e5c2",
+    "url": "/clickygame/static/css/main.86ac3946.chunk.css"
+  },
+  {
+    "revision": "6eea88aaae0981f33b1c9fa04cb4648c",
+    "url": "/clickygame/index.html"
+  }
+];
